af7a6af47695374c1358c7a985b44e8b8df0968f
FuntyGithub <74513280+FuntyGithub@users.noreply.github.com>
2022-11-13 00:26:39 +0100

    bisschen was vervollständigt .-.

--- src/script.js
+++ src/script.js
@@ -14,19 +14,22 @@ window.onload = function () {
         // get query string values
         var text = decodeURIComponent(queryObject.text);
         var maxWidth = queryObject.maxWidth ? queryObject.maxWidth : 500;
+        var textColor = queryObject.textColor ? queryObject.textColor : "black";
+        var bgColor = queryObject.bgColor ? queryObject.bgColor : "white";
+        var font = queryObject.font ? queryObject.font : "30px Arial";
         
         // 
         if(queryObject.type === "raw"){
             
             // make website only return data url
-            document.body.innerHTML = textToImg(maxWidth, text);
+            document.body.innerHTML = textToImg(maxWidth, text, textColor, bgColor, font);
 
         } else {
 
             // make website only return image
             document.body.innerHTML = "";
             var img = document.createElement("img");
-            img.src = textToImg(maxWidth, text);
+            img.src = textToImg(maxWidth, text, textColor, bgColor, font);
             document.body.appendChild(img);
         }
 
@@ -36,17 +39,20 @@ window.onload = function () {
 
             var maxWidth = document.getElementById("maxWidth").value;
             var text = document.getElementById("text").value;
+            var textColor = document.getElementById("textColor").value;
+            var bgColor = document.getElementById("bgColor").value;
+            var font = document.getElementById("font").value;
 
             // create image
             var img = document.createElement("img");
-            img.src = textToImg(maxWidth, text);
+            img.src = textToImg(maxWidth, text, textColor, bgColor, font);
             document.getElementById("img").appendChild(img);
 
         }
 
         document.getElementById("img").onclick = function () {
 
-            // open image as bufferd file in new tab
+            // open image as image in new tab
             var img = document.getElementById("img").getElementsByTagName("img")[0];
             var win = window.open();
             win.document.write(img.outerHTML);
@@ -56,41 +62,71 @@ window.onload = function () {
 }
 
 
-function textToImg(maxWidth, text) {
+function textToImg(maxWidth, text, textColor, bgColor, font) {
+
+    // set font
+    font = font ? font : "30px Arial";
+    var fontHeight = parseInt(font.split("px")[0]);
 
     // text to image
     var canvas = document.createElement("canvas");
     var context = canvas.getContext("2d");
-    context.font = "30px Arial";
+    context.font = font;
     var width = context.measureText(text).width;
 
     // new line if text is too long for maxWidth
-    if(width > maxWidth){
-        var words = text.split(" ");
-        var line = "";
-        var lines = [];
-        for(var n = 0; n < words.length; n++) {
-            var testLine = line + words[n] + " ";
-            var testWidth = context.measureText(testLine).width;
-            if (testWidth > maxWidth) {
-                lines.push(line);
-                line = words[n] + " ";
-            }
-            else {
-                line = testLine;
-            }
+    // if(width > maxWidth){
+    //     var words = text.split(" ");
+    //     var line = "";
+    //     var lines = [];
+    //     for(var n = 0; n < words.length; n++) {
+    //         var testLine = line + words[n] + " ";
+    //         var testWidth = context.measureText(testLine).width;
+    //         if (testWidth > maxWidth) {
+    //             lines.push(line);
+    //             line = words[n] + " ";
+    //         }
+    //         else {
+    //             line = testLine;
+    //         }
+    //     }
+    //     lines.push(line);
+    // }
+
+    var words = text.split(" ");
+    var line = "";
+    var lines = [];
+    for(var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var testWidth = context.measureText(testLine).width;
+        if (testWidth > maxWidth) {
+            lines.push(line);
+            line = words[n] + " ";
         }
-        lines.push(line);
+        else {
+            line = testLine;
+        }
     }
+    lines.push(line);
 
     // draw text
     canvas.width = width < maxWidth ? width : maxWidth;
-    canvas.height = 30 * lines.length;
-    context.font = "30px Arial";
-    context.fillStyle = "black";
+    canvas.height = fontHeight * lines.length + 10;
+    context.font = font;
+    
+    // background color
+    if (bgColor === "transparent") {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+    } else {
+        context.fillStyle = bgColor;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    // text color
+    context.fillStyle = textColor ? textColor : "black";
     
     for(var n = 0; n < lines.length; n++) {
-        context.fillText(lines[n], 0, 30 * (n + 1));
+        context.fillText(lines[n], 0, fontHeight * (n + 1));
     }
 
 
